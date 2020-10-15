8b67040e160e559f18a7f0f49de9b868d4d600f5
arlan <akayev23@fizmat.kz>
2020-10-15 16:39:23 +0600

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+//alert("hello world")
+//confirm("ты гей?")
+//prompt("who are you")
+let a = 10,
+	b = 13;
+console.log(a--)
+console.log(b++)
+
+
+let istrue = true;
+let isfalse = false;
+
+
+c = "hello";
+console.log(c)
+
+
